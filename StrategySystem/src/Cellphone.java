0ee695c028cfb36ef211b41e345cd0cf61be2737
andrew <andrew.cote00@gmail.com>
2013-10-28 19:12:16 -0700

    added phone and number options

---
+++ StrategySystem/src/Cellphone.java
@@ -0,0 +1,6 @@
+
+public class Cellphone {
+public void enterNumber(int numbertoenter){
+	System.out.println("Number entered is " + numbertoenter);
+}
+}
--- StrategySystem/src/Hello.java
+++ StrategySystem/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello world");
+		Cellphone myPhone;
+		myPhone = new Cellphone ();
+		myPhone.enterNumber(5);
+		
 	}
 }
