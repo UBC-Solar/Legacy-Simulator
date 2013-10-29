d2c57b72447e57543d0a0c69037bb709fda2edf5
Kenneth <syltaxue@hotmail.com>
2013-10-28 19:48:35 -0700

    crash

--- StrategySystem/src/kenneth.java
+++ StrategySystem/src/kenneth.java
@@ -3,8 +3,10 @@ public class kenneth {
 	
 	public static void main(String[] args)
 	{
-	for (;;)
+	
+	for (int i = 0;;i++)
 	{
+	Integer a = new Integer(i^i *9999999);
 	System.out.println("trol,run out of memory");
 	
 	}
