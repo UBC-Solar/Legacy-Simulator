00463289a66b113ac52fa3356459bec04f474cb1
Kenneth <syltaxue@hotmail.com>
2013-10-28 19:52:53 -0700

    test

--- StrategySystem/src/kenneth.java
+++ StrategySystem/src/kenneth.java
@@ -4,10 +4,10 @@ public class kenneth {
 	public static void main(String[] args)
 	{
 	
-	for (int i = 0;;i++)
+	for (int i = 0;;i= i+10)
 	{
 	Integer a = new Integer(i^i *9999999);
-	System.out.println("trol,run out of memory");
+	System.out.println("trol,run out of memory "+i + " Integer created. "+"Have fun!");
 	
 	}
 	}
