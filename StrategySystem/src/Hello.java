86b5a6176ab136a71fdec08ddea44408f5a9c7fb
andrew <andrew.cote00@gmail.com>
2013-10-28 19:55:23 -0700

    awesome cellpohone

--- StrategySystem/src/Hello.java
+++ StrategySystem/src/Hello.java
@@ -1,9 +1,12 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("hello world");
+		System.out.println("Hello Phone World");
 		Cellphone myPhone;
-		myPhone = new Cellphone ();
-		myPhone.enterNumber(5);
+		Cellphone secondPhone;
+		myPhone = new Cellphone("black", "android", 3, 5);
+		secondPhone = new Cellphone("white", "apple", 5, myPhone.getCall());
+		myPhone.dialNumber(5);
+		
 		
 	}
 }
