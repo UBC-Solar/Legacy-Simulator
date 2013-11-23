02f68431cbc5d7aeb7068223808275dd6d2cb963
Winnie <miao.winnie@gmail.com>
2013-11-23 12:35:11 -0800

    Fixed comments

--- StrategySystem/src/com/ubcsolar/sim/Battery.java
+++ StrategySystem/src/com/ubcsolar/sim/Battery.java
@@ -5,12 +5,6 @@ This class models the battery. The important features are:
 - display the battery's temperature and state of charge at set time iterations
 - show error if it overheats
 - show error if it overcharges
-
-Drawing & storing power is done through other methods.
-
-Interacts mainly with the electric controller:
-- lets the electric controller know how long the batteries could charge for, until it reaches 100%
-- lets the electric controller know what current the batteries can provide, given the voltage and time period demanded
 */
 
 
@@ -30,6 +24,7 @@ private double time;
 /** Constructs a default battery, at 100% charged state.
  *  Values for maxChargeCapacity & batteryVoltage are taken from manufacturer's product data sheet.
  *  @todo Confirm this assumption: total stored electric energy in batteries is given by:  Ah*V = Wh		(charge capacity*voltage = energy)
+ *  @todo Confirm this assumption: that the battery's voltage starts out at 40.15V
  */
 public Battery(){
 	maxChargeCapacity = 220;				 
@@ -66,48 +61,57 @@ public Battery(String fileName){
 //-----------------------------------------CALCULATION METHODS------------------------------------------------------------------------------
 /** Calculates the maximum time the battery could recharge for, before it overcharges. (in seconds)
  * Should display error if the specified charge is over the battery's max charge capacity
- * @param voltage - specified by the electrical controller (in volts)
  * @param currentRecharge - the current the battery will be charged at, specified by the electrical controller (in amps)
  * @todo Confirm the battery's max charge capacity, and confirm that it absolutely cannot charge at a capacity that exceeds it
  * @todo Confirm this assumption: the charging time is given by: (maxWh-currentWh)/(V*A)
  * @todo Incorporate charging efficiency, temperature, other factors that will affect the charging time
  * */
-public double getMaxRechargeTime(double voltage, double currentRecharge){
+public double getMaxRechargeTime(double currentRecharge){
 	double rechargeTime;
 	if (currentRecharge > maxChargeCapacity){
 		Log.write("Cannot exceed max charge capactiy");
 		return -1.0;				
 	}
 	else{
-		rechargeTime =(maxStoredEnergy-storedEnergy)/(voltage*currentRecharge )*3600;
+		rechargeTime =(maxStoredEnergy-storedEnergy)/(batteryVoltage*currentRecharge )*3600;
 		return rechargeTime;
 	}
 }
 
 /** Calculates what current the battery can provide (in amps), considering it's current state of charge.
- * @param voltage - specified by the electrical controller
  * @param chargeTime - time period (in seconds) for which the battery will be providing this current; specified by electrical controller
  * @todo Confirm this assumption: Wh/(V*t) = A
  * @todo Incorporate charging efficiency, temperature, other factors that will be of influence
  * */
-public double getCurrent(double voltage, double chargeTime){ 
+public double getCurrent(double chargeTime){ 
 	double currentCharge;
-	currentCharge = storedEnergy/(chargeTime/3600 * voltage);
+	currentCharge = storedEnergy/(chargeTime/3600 * batteryVoltage);
 	return currentCharge;
 }
 
+/** Calculates heat produced from the charge/discharge
+ * Should consider ambient temperature, battery's temperature, heat from adjacent batteries in the pack
+ * @todo Need to add calculations
+ */
 public double heatFromCharge(double current, double chargeTime){
 	double heatFromCharge=0;
-	// TO DO: need calculations here
-	// need to consider ambient temperature, battery's temperature, and the heat produced by the adjacent batteries in the pack
+	// need calculations here
 	return heatFromCharge;
 }
 
-public void storeEnergy(double time, double current, double voltage){					// Store energy (in watt-hours)
+/** Stores energy (in watt-hrs)
+ * @todo Incorporate efficiency
+ * @todo Incorporate calculates that changes the batteryVoltage since voltage varies according to battery's state of charge
+ */
+public void storeEnergy(double time, double current, double voltage){
 	storedEnergy = storedEnergy + (current*voltage*time)/3600;	
 }
 
-public void drawEnergy(double time, double current, double voltage){					// Draw energy (in watt-hours)
+/** Draws energy (in watt-hrs)
+ * @todo Incorporate efficiency
+ * @todo Incorporate calculates that changes the batteryVoltage since voltage varies according to battery's state of charge
+ */
+public void drawEnergy(double time, double current, double voltage){
 	storedEnergy = storedEnergy - (current*voltage*time)/3600;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------
