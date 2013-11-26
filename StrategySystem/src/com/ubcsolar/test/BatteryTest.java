7746689c7c4b7d8e612857d1465cbbb0c336a40f
Winnie <miao.winnie@gmail.com>
2013-11-25 19:43:48 -0800

    fixed storeEnergy & drawEnergy

--- StrategySystem/src/com/ubcsolar/test/BatteryTest.java
+++ StrategySystem/src/com/ubcsolar/test/BatteryTest.java
@@ -15,34 +15,66 @@ public class BatteryTest {
 	public void init(){
 	myTestBattery = new Battery();
 }
+	
+	@Test
+	public void init2(){
+		myTestBattery = new Battery("myfile.txt");
+	}
+	
+	public void test2(){
+		init2();
+		test();
+		init2();
+		testGetBatteryVoltage();
+		init2();
+		testGetStateOfCharge();
+		init2();
+		testGetMaxRechargeTime();
+		init2();
+		testGetCurrent();
+		init2();
+		testStoreEnergy();
+		init2();
+		testDrawEnergy();
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	
 	@Test
-	public void test1GetMaxRechargeTime(){
+	public void testGetBatteryVoltage(){
+		assertTrue(myTestBattery.getBatteryVoltage()== 40.15);
+	}
+	
+	
+	
+	@Test
+	public void testGetStateOfCharge(){
+		assertNotNull(myTestBattery.getStateOfCharge());
+		assertTrue(myTestBattery.getStateOfCharge()== (int)((220*40.15/2)/(220*40.15))*100);
+	}
+	
+	@Test
+	public void testGetMaxRechargeTime(){
 		assertTrue(myTestBattery.getMaxRechargeTime(10.0)== 39600.0);
+		assertTrue(myTestBattery.getMaxRechargeTime(230.0)== -1.0);
 	}
 	
 	@Test
-	public void test2GetMaxRechargeTime(){
-		assertTrue(myTestBattery.getMaxRechargeTime(30.0)== -1.0);
-	}
-	
-	@Test
-	public void test1GetCurrent(){
+	public void testGetCurrent(){
 		assertTrue(myTestBattery.getCurrent(5.0)== 79200.0);
 	}
 	
 	@Test
 	public void testStoreEnergy(){
-		assertTrue(myTestBattery.storeEnergy(5.0,10.0)== 4416.5);
+		assertTrue(myTestBattery.storeEnergy(5.0,10.0)> 4416.5);
 	}
 	
 	@Test
 	public void testDrawEnergy(){
-		assertTrue(myTestBattery.drawEnergy(5.0,10.0)== 4416.5);
+		assertTrue(myTestBattery.drawEnergy(5.0,10.0)< 4416.5);
 				
 	}
 	
