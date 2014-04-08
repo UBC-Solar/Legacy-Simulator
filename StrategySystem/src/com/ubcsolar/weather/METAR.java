444c340082ac01a5f2764a2a12a9044df7e457d1
Winnie <miao.winnie@gmail.com>
2014-04-07 18:57:16 -0700

    jkh

--- StrategySystem/src/com/ubcsolar/weather/METAR.java
+++ StrategySystem/src/com/ubcsolar/weather/METAR.java
@@ -1,33 +1,109 @@
 package com.ubcsolar.weather;
 
 public class METAR {
-//private final Long rawText;
+private final Long rawText;
 private final String stationID;
-//private final Long observationTime;
-private final Long time;
-/*
-private final latitude;
-private final longitude;
-
-
-*/
+private final Long observationTime;
+private final double latitude;
+private final double longitude;
+private final double tempC;
+private final double dewPointC;
+private final int windDirection;
 private final int windSpeed;
-public METAR(Long time, String airID, int windSpeed){
-	this.time = time;
-	this.stationID = airID;
+private final double visibilityStatute;
+private final double altim;
+private final double seaLevelPressure;
+private final String flightCategory;
+private final double pressureTendency3hr;
+private final String metarType;
+private final double elevation;
+
+
+public METAR(Long rawText, String stationID, Long time, double latitude, double longitude, double temp, double dewPoint, int windDir, 
+		int windSpeed, double visibility, double altim, double seaLevelPressure, String flightCategory, double pressureTendency, 
+		String metarType, double elevation){
+	this.rawText = rawText;
+	this.stationID = stationID;
+	this.observationTime = time;
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.tempC = temp;
+	this.dewPointC = dewPoint;
+	this.windDirection = windDir;
 	this.windSpeed = windSpeed;
+	this.visibilityStatute = visibility;
+	this.altim = altim;
+	this.seaLevelPressure = seaLevelPressure;
+	this.flightCategory = flightCategory;
+	this.pressureTendency3hr = pressureTendency;
+	this.metarType = metarType;
+	this.elevation = elevation; 	
 }
-public int getWindSpeed() {
-	return windSpeed;
+
+
+public Long getRawText(){
+	return rawText;
 }
-public String getAirportID() {
+
+public String getStationID(){
 	return stationID;
 }
-public Long getTime() {
-	return time;
+
+public Long getObsvTime(){
+	return observationTime;
+}
+
+public double getLatitude(){
+	return latitude;
+}
+
+public double getLongitude(){
+	return longitude;
+}
+
+public double temp(){
+	return tempC;
+}
+
+public double dewPoint(){
+	return dewPointC;
+}
+
+public int windDirection(){
+	return windDirection;
 }
 
 
+public int windSpeed(){
+	return windSpeed;
+}
+	
+public double visibility(){
+	return visibilityStatute;
+}
+
+public double altim(){
+	return altim;
+}
+
+public double seaLevelPressure(){
+	return seaLevelPressure;
+}
+
+public String flightCategory(){
+	return flightCategory;
+}
+
+public double pressureTendency(){
+	return pressureTendency3hr;
+}
+
+public String metarType(){
+	return metarType;
+}
+public double elevation(){
+	return elevation;
+}
 
 
 }
