b9550c6dd5a7e3aee19575239c5acc212aa9d5f4
Noah <noah.sommerfeld@gmail.com>
2014-05-15 19:41:30 -0700

    Elevation profile map working!!! Mostly hardcoded, but working!

--- StrategySystem/src/com/ubcsolar/ui/Map.java
+++ StrategySystem/src/com/ubcsolar/ui/Map.java
@@ -45,7 +45,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
 import org.xml.sax.SAXException;
@@ -150,7 +152,7 @@ public class Map extends JFrame implements Listener {
 				
 					try {
 						//TODO hardcoded, will need to update
-						mySession.getMapController().load("res/test.txt");
+						mySession.getMapController().load("res/NEWEDCToHopeElevations.kml");
 						
 					} catch (IOException e) {
 						JDialog dialog = new ErrorMessage("IO Exception: File could not be loaded (bad filename?)");
@@ -241,23 +243,113 @@ public class Map extends JFrame implements Listener {
 	 */
 	private void updateMap(ArrayList<Point> listOfPoints,
 			int numOfDistanceRequested, DistanceUnit unitMeasuredBy) {
-		
-		if(listOfPoints.size() <1){ //if there are no points, abort. 
+		contentPane.remove(this.cp);
+		//if there are no points, make an empty graph?
+		/*if(listOfPoints.size() <1){ //if there are no points, abort. 
 			return;
+		}*/
+		
+		//TODO make the unit dynamic.
+		//ds = makeDataSet(listOfPoints, DistanceUnit.KILOMETERS);
+		//elevationChart.
+		
+		double minHeight = 0.0; //blank chart
+		double maxHeight = 200.0; //a blank chart
+		
+		//TODO ignores last 2 cuz I haven't figured out how to sort them yet. 
+				double tripDistance = 0.0; //whatever units I select below. 
+				double[][] data = new double[2][listOfPoints.size()-2];
+				if(listOfPoints.size() > 1){
+					data[0][0] = tripDistance; //first distance is zero.
+					data[1][0] = listOfPoints.get(0).getElevationInFeet();
+					minHeight = listOfPoints.get(0).getElevationInFeet();
+					maxHeight = listOfPoints.get(0).getElevationInFeet();
+					
+					for(int i = 1; i<listOfPoints.size()-2; i++){
+						tripDistance +=  listOfPoints.get(i-1).calculateDistance(listOfPoints.get(i), unitMeasuredBy);
+						data[0][i] = tripDistance;
+						
+						//TODO make this change from feet to anything
+						double tempHeight = listOfPoints.get(i).getElevationInFeet();
+						data[1][i] = tempHeight;
+						if(tempHeight - minHeight < 0){
+							minHeight = tempHeight; //new low
+						}
+						if(tempHeight - maxHeight > 0){
+							maxHeight = tempHeight; //new height
+						}
+						
+						
+					}
+					
+					
+				}
+				
+				
+				DefaultXYDataset dds = new DefaultXYDataset();
+				
+				dds.addSeries("Route", data);
+				ds = dds;
+				
+		
+		this.elevationChart = 
+				ChartFactory.createXYLineChart(
+						"Height Map",
+						"Travel Distance (" + unitMeasuredBy + ")",
+						"elevation (feet)", //TODO make this dynamic 
+						ds,
+						PlotOrientation.VERTICAL, true, true, false);
+		
+		XYPlot plot = (XYPlot) elevationChart.getPlot();
+		ValueAxis axis = plot.getRangeAxis();
+		axis.setLowerBound(minHeight - ((maxHeight - minHeight) * 0.1)); //pad by 10% of the difference
+		axis.setUpperBound(maxHeight + ((maxHeight - minHeight) *  0.1)); //pad by 10% for prettiness
+		
+		
+		
+		cp = new ChartPanel(elevationChart);
+	
+		
+		
+		
+		contentPane.add(cp);
+		cp.repaint();
+		cp.revalidate();
+		contentPane.revalidate();
+		
+		contentPane.repaint();
+		
+		//initialize ds, elevationChart, and cp
+	
+		
+	}
+
+
+
+	private XYDataset makeDataSet(ArrayList<Point> listOfPoints, DistanceUnit elevationUnit) {
+		//TODO ignores last 2 cuz I haven't figured out how to sort them yet. 
+		double tripDistance = 0.0; //whatever units I select below. 
+		double[][] data = new double[2][listOfPoints.size()-2];
+		if(listOfPoints.size() > 1){
+			data[0][0] = tripDistance; //first distance is zero.
+			data[1][0] = listOfPoints.get(0).getElevationInFeet();
+			
+			for(int i = 1; i<listOfPoints.size()-2; i++){
+				tripDistance +=  listOfPoints.get(i-1).calculateDistance(listOfPoints.get(i), elevationUnit);
+				data[0][i] = tripDistance;
+				
+				//TODO make this change from feet to anything
+				data[1][i] = listOfPoints.get(i).getElevationInFeet();
+			}
+			
+			
 		}
-		//TODO this ignores the last 2, because I haven't figured out how to sort them yet. 
-		double[][] data = new double[listOfPoints.size() -2][listOfPoints.size()-2];
 		
 		
-		
-		/*
 		DefaultXYDataset dds = new DefaultXYDataset();
 		
-		
-		//double[][] data = { {0.1, 0.2, 0.3}, {1, 2, 3} };
-		
-		dds.addSeries("series1", data);
-		return dds;*/
+		dds.addSeries("Route", data);
+		return dds;
 		
 	}
 	
