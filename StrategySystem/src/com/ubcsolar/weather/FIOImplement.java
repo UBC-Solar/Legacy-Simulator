a1d8a172758c6703fe9ba2a7557ea739fd3821a9
rhlsnk <rhlsnk@gmail.com>
2015-04-11 12:56:16 -0700

    Added getter methods for FIOWeather class

--- StrategySystem/src/com/ubcsolar/weather/FIOImplement.java
+++ StrategySystem/src/com/ubcsolar/weather/FIOImplement.java
@@ -6,9 +6,10 @@ import com.github.dvdme.ForecastIOLib.ForecastIO;
 public class FIOImplement {
 	
 	public String API_KEY = "49358bec9be8ea1393625a7334124152"; // API-KEY do not edit.
-	public String Latitude ="38.7252993";// Enter Latitude here
-	public String Longitude ="-9.1500364";// Enter Longitude here	
+	public String Latitude ="49.2625160";// Enter Latitude here
+	public String Longitude ="-123.2463750";// Enter Longitude here	
 	private ForecastIO fio;
+	private FIOCurrently currently;
 	public static void main(String[] args){
 		FIOImplement test = new FIOImplement();
 		test.print();
@@ -18,28 +19,41 @@ public class FIOImplement {
 	this.fio = new ForecastIO(API_KEY);
 	fio.setUnits(ForecastIO.UNITS_SI);
 	fio.setLang(ForecastIO.LANG_ENGLISH);
-	fio.getForecast("38.7252993", "-9.1500364");
+	fio.getForecast(Latitude, Longitude);
 	}
 	
 	
+	public void currentlyReport(){
+		currently = new FIOCurrently(fio);
+		} 
+	//Can put this inside constructor, should I??
 	
 	public void print(){
-	
+		
 		System.out.println("Latitude: " + fio.getLatitude());
 		
-		FIOCurrently currently = new FIOCurrently(fio);
+		currentlyReport();
 	    //Print currently data
 		System.out.println("\nCurrently\n");
 		String [] f  = currently.get().getFieldsArray();
 		for(int i = 0; i<f.length;i++){
 			System.out.println(f[i]+": "+currently.get().getByKey(f[i]));
 		}
+		
 }
 	
-	//TODO: getter methods, for weather
-	//TODO: data holder class: figure out the relationship between controller, acquire and the db
-	//TODO: have it source independent
-	//TODO: also notification for hey we have a new weather report 
+//TODO: Getter methods for the weather
+//TODO: Should have a list for currently 
+//TODO: Should have a list for daily 
+	
+//TODO: Should have a cloudCover, cloudCoverError, what other methods would be req'd?
+	
+//TODO: Data holder class: figure out the relationship between controller, acquire and the database
+//TODO: Also notification for hey we have a new weather report
+	
+	
+//TODO: Eventually this software should be source independent
+//TODO: Maybe create a counter of all weather reports created, so that we don't go past our limit
 	
 	
 	
