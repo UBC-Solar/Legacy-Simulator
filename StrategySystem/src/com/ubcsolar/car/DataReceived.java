51379bc600bb0aa988a9352c85c96b5c308685e5
jcj83429 <jcj83429@gmail.com>
2015-05-09 13:19:13 -0700

    DataReceived: added getters.

--- StrategySystem/src/com/ubcsolar/car/DataReceived.java
+++ StrategySystem/src/com/ubcsolar/car/DataReceived.java
@@ -11,6 +11,32 @@ public class DataReceived {
 	public Map<String,Integer> temperatures = new HashMap<String,Integer>();
 	public Map<Integer,ArrayList<Float>> cellVoltages = new HashMap<Integer,ArrayList<Float>>();
 	
+	public int getSpeed(){
+		return speed;
+	}
+	
+	public float getTotalVoltage(){
+		return totalVoltage;
+	}
+	
+	public int getStateOfCharge(){
+		return stateOfCharge;
+	}
+	
+	public Map<String,Integer> getTemperatures(){
+		Map<String,Integer> copyTemperatures = new HashMap<String,Integer>();
+		copyTemperatures.putAll(temperatures);
+		return copyTemperatures;
+	}
+	
+	public Map<Integer,ArrayList<Float>> getCellVoltages(){
+		Map<Integer,ArrayList<Float>> copyCellVoltages = new HashMap<Integer,ArrayList<Float>>();
+		for(Integer key : cellVoltages.keySet()){
+			copyCellVoltages.put(key, new ArrayList<Float>(cellVoltages.get(key)));
+		}
+		return copyCellVoltages;
+	}
+	
 	public String toString(){
 		return Integer.toString(this.speed) + "\n"
 				+ Float.toString(this.totalVoltage) + "\n"
