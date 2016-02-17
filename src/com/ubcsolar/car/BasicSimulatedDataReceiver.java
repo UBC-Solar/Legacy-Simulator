31cbdf9057dfb02e427d96735b7c0f9e1c9a3b0d
jcj83429 <jcj83429@gmail.com>
2016-02-17 15:03:08 -0800

    less crappy fake car data

--- src/com/ubcsolar/car/BasicSimulatedDataReceiver.java
+++ src/com/ubcsolar/car/BasicSimulatedDataReceiver.java
@@ -19,9 +19,18 @@ public class BasicSimulatedDataReceiver extends AbstractDataReceiver {
 
 	private String name;
 	private Timer autoGenerate;
+	
+	private double temps[] = new double[6];
+	private double speed, totalV;
+	
+	
 	public BasicSimulatedDataReceiver(DataProcessor myProcessor, CarController toAdd){
 		super(toAdd, myProcessor); 
 		name = "basic sim";
+		for(int i=0; i<6; i++)
+			temps[i] = 25.0;
+		speed = 0;
+		totalV = 44;
 	}
 	
 	private void receiveNewPacket(TelemDataPacket newPacket){
@@ -114,36 +123,53 @@ private class generateNewThings extends TimerTask{
 	 * @return the next TelemDataPacket in the series. 
 	 */
 	private TelemDataPacket generateNewTelemDataPack(){
-		
-		int speed = 1 * iterations; //terrible algo. Please improve me. 
-									//Car should not be going faster than light after 
-									//running this program 1x1^23 iterations. 
-		int totalVoltage = 1 * iterations; //also terrible algo. 
+		Random rng = new Random();
+		speed = speed + rng.nextFloat()*2 - 1;
+		if(speed < 0)
+			speed = 0;
+		totalV = totalV + rng.nextFloat()*0.2 - 0.1;
+		totalV = totalV > 50 ? 50 : totalV < 40 ? 40 : totalV;
+		for(int i=0; i<6; i++)
+			temps[i] += rng.nextFloat() - 0.5;
 		
 		HashMap<String,Integer> temperatures = new HashMap<String,Integer>();
-		temperatures.put("bms", (35 + iterations));
-		temperatures.put("motor", (40 + iterations));
-		temperatures.put("pack0", (41+ iterations));
-		temperatures.put("pack1", (42 + iterations));
-		temperatures.put("pack2", (43 + iterations));
-		temperatures.put("pack3", (44 + iterations));
+		temperatures.put("bms", (int) temps[0]);
+		temperatures.put("motor", (int) temps[1]);
+		temperatures.put("pack0", (int) temps[2]);
+		temperatures.put("pack1", (int) temps[3]);
+		temperatures.put("pack2", (int) temps[4]);
+		temperatures.put("pack3", (int) temps[5]);
 		HashMap<Integer,ArrayList<Float>> cellVoltages = new HashMap<Integer,ArrayList<Float>>();
 		for(int i = 0; i<4; i++){ //Current number of cells coming in pack is 4. Will probably have to adjust that.
-			cellVoltages.put(i, generatePackVoltages());
+			cellVoltages.put(i, generatePackVoltages(totalV));
 		}
-		TelemDataPacket tempPacket = new TelemDataPacket(speed, totalVoltage, temperatures, cellVoltages);
+		TelemDataPacket tempPacket = new TelemDataPacket((int)speed, (int)totalV, temperatures, cellVoltages);
 		iterations++;
 		return tempPacket;
 	}
 	
-	private ArrayList<Float> generatePackVoltages(){
+	private ArrayList<Float> generatePackVoltages(double totalV){
 		ArrayList<Float> cell1 = new ArrayList<Float>();
 		Random rng = new Random();
 		
-		for(int i = 0; i<10; i++){
-			cell1.add(rng.nextFloat() * 10);
-		}
+		putPackVoltages(10, totalV, cell1);
 		return cell1;
+	}
+	
+	private void putPackVoltages(int nCells, double totalV, ArrayList<Float> voltages){
+		if(nCells == 0){
+			return;
+		}else if(nCells == 1){
+			voltages.add((float)totalV);
+			return;
+		}
+		
+		Random rng = new Random();
+		int half1 = nCells / 2;
+		double middleV = totalV * half1 / nCells;
+		middleV = middleV * (1.0 + 0.2 * rng.nextFloat() + 0.1);
+		putPackVoltages(half1, middleV, voltages);
+		putPackVoltages(nCells-half1, totalV-middleV, voltages);
 	}
 }
 
