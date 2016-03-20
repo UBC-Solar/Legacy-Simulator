d2878f797160d94641f586543e456649f21a3362
Hooman <Hooman@192.168.0.14>
2016-03-19 21:17:35 -0700

    API_Keys class added.

---
+++ src/com/ubcsolar/Main/API_Keys.java
@@ -0,0 +1,17 @@
+package com.ubcsolar.Main;
+
+public class API_Keys {
+	
+	//Hooman's key
+	public final static String GOOGLE_MAPS_KEY = "AIzaSyDYE2CgPSZGLTJWMSaNSg4woYrZjJ_qwXk";
+	
+	//Strategy@ubcsolar.com's Key
+//	public final static String GOOGLE_MAPS_KEY = "AIzaSyCMCYQ_X_BgCcGD43euexoiIJED__44mek";
+	
+	
+	
+	
+	//Strategy@ubcsolar.com's key
+	public final static String WEATHER_KEY= "e4f99878991a762e85efbffc7db8d657";
+
+}
