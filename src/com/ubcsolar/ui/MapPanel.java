83300ee7cdf191f79206a81adad9026f797aeeef
Will <will.beh@ubcsolar.com>
2016-05-28 18:41:14 -0700

    Map Panel now displays map loaded

--- src/com/ubcsolar/ui/MapPanel.java
+++ src/com/ubcsolar/ui/MapPanel.java
@@ -9,9 +9,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.ubcsolar.Main.GlobalController;
 import com.ubcsolar.common.Listener;
 import com.ubcsolar.common.LogType;
 import com.ubcsolar.common.SolarLog;
+import com.ubcsolar.notification.NewMapLoadedNotification;
 import com.ubcsolar.notification.Notification;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
@@ -20,9 +22,12 @@ import java.awt.BorderLayout;
 
 public class MapPanel extends JPanel implements Listener {
 	
-	private GUImain parent;
-	
-	public MapPanel(GUImain main) {
+	protected GUImain parent;
+	private GlobalController mySession;
+	private JLabel label_1;
+
+	public MapPanel(GUImain main, GlobalController session) {
+	mySession = session;
 	parent = main;
 	this.setBorder(BorderFactory.createLineBorder(Color.black));
 	setLayout(new BorderLayout(0, 0));
@@ -34,33 +39,58 @@ public class MapPanel extends JPanel implements Listener {
 		panel.add(label);
 		
 		JButton button = new JButton("Advanced");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				parent.launchMap();
+			}
+		});
 		panel.add(button);
+
 		
 		JPanel panel_1 = new JPanel();
 		add(panel_1, BorderLayout.CENTER);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		
 		JLabel lblMapLoaded = new JLabel("Map Loaded: ");
 		GridBagConstraints gbc_lblMapLoaded = new GridBagConstraints();
+		gbc_lblMapLoaded.insets = new Insets(0, 0, 0, 5);
 		gbc_lblMapLoaded.gridx = 3;
 		gbc_lblMapLoaded.gridy = 6;
 		panel_1.add(lblMapLoaded, gbc_lblMapLoaded);
-	register(); //do last, in case a notification is sent before we're done building.
-	
+		
+		label_1 = new JLabel("");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.insets = new Insets(0, 0, 0, 5);
+		gbc_label_1.gridx = 5;
+		gbc_label_1.gridy = 6;
+		panel_1.add(label_1, gbc_label_1);
+		
+		register(); //do last, in case a notification is sent before we're done building.
+		
+		updateMapLabel(mySession.getMapController().getLoadedMapName());
+		
 	}
+
+	private void updateMapLabel(String mapName){
+		label_1.setText(mapName);
+	}
+	
 	@Override
 	public void notify(Notification n) {
 		// TODO Auto-generated method stub
-		
+		if(n.getClass() == NewMapLoadedNotification.class){
+		updateMapLabel(((NewMapLoadedNotification) n).getMapLoadedName());
+		}
 	}
 	@Override
 	public void register() {
 		// TODO Auto-generated method stub
+		mySession.register(this,  NewMapLoadedNotification.class);
 		
 	}
 }
