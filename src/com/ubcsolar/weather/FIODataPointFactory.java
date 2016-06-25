9ee04514226662827d91f872b0347446aa150808
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-06-25 13:53:08 -0700

    more on ForecastIOFactory2

---
+++ src/com/ubcsolar/weather/FIODataPointFactory.java
@@ -0,0 +1,132 @@
+package com.ubcsolar.weather;
+
+import com.eclipsesource.json.JsonObject;
+import com.ubcsolar.common.GeoCoord;
+
+public class FIODataPointFactory {
+	//changeable by user
+		
+		private double precipProbability = 0;
+		private String precipType = "rain";
+		private double temperature = 0;
+		private double apparentTemperature = 0;
+		private double dewPoint = 0;
+		private double humidity = 0;
+		private double windSpeed = 0;
+		private double windBearing = 0;
+		private double cloudCover = 0;
+		private double stormBearing = 0;
+		private double stormDistance = 0;
+
+		
+		//fixed
+		
+		private String summary = "TEST";
+		private String icon;
+		private int time = 0;
+		private double precipIntensity = 0;
+		private double visibility = 0;
+		private double pressure = 0;
+		private double ozone = 0;
+		
+		private JsonObject data;
+		
+		public FIODataPointFactory temperature(double temperature){
+			this.temperature = temperature;
+			this.apparentTemperature = temperature;
+			return this;
+		}
+		
+		public FIODataPointFactory cloudCover(double cloudCover){
+			this.cloudCover = cloudCover / 100;
+			return this;
+		}
+		
+		public FIODataPointFactory dewPoint(double dewPoint){
+			this.dewPoint = dewPoint;
+			return this;
+		}
+		
+		public FIODataPointFactory humidity(double humidity){
+			this.humidity = humidity;
+			return this;
+		}
+		
+		public FIODataPointFactory stormDistance (double stormDistance ){
+			this.stormDistance = stormDistance;
+			return this;
+		}
+		
+		public FIODataPointFactory stormBearing (double stormBearing ){
+			this.stormBearing = stormBearing;
+			return this;
+		}
+		
+		public FIODataPointFactory windSpeed(double windSpeed){
+			this.windSpeed = windSpeed;
+			return this;
+		}
+		
+		public FIODataPointFactory windBearing(double windBearing){
+			this.windBearing = windBearing;
+			return this;
+		}
+		
+		public FIODataPointFactory precipProb(double precipitationProbability){
+			this.precipProbability = precipitationProbability / 100;
+			return this;
+		}
+		
+		public FIODataPointFactory precipType(String precipType){
+			this.precipType = precipType;
+			return this;
+		}
+		
+		/**
+		 * creates a JsonObject with all of the data given to the dataPoint. If
+		 * data is missing, it will be entered as 0
+		 * @return the JsonObject with all of the data
+		 */
+		
+		public JsonObject build(){
+			chooseIcon();
+			
+			data = new JsonObject();
+			data.add("time", time);
+			data.add("summary", summary);
+			data.add("icon", icon);
+			data.add("precipIntensity", precipIntensity);
+			data.add("precipProbability", precipProbability);
+			data.add("precipType", precipType);
+			data.add("temperature", temperature);
+			data.add("apparentTemperature", apparentTemperature);
+			data.add("dewPoint", dewPoint);
+			data.add("humidity", humidity);
+			data.add("windSpeed", windSpeed);
+			data.add("windBearing", windBearing);
+			data.add("nearestStormDistance", stormDistance);
+			data.add("nearestStormBearing", stormBearing);
+			data.add("visibility", visibility);
+			data.add("cloudCover", cloudCover);
+			data.add("pressure", pressure);
+			data.add("ozone", ozone);
+			
+			return data;
+		}
+		
+		public void chooseIcon(){
+			if(precipProbability >= 80){
+				icon = "hella rainy";
+			}else if(precipProbability >= 40){
+				icon = "rainy";
+			}else if(precipProbability >= 10){
+				icon = "slightly rainy";
+			}else if(cloudCover >= 50){
+				icon = "cloudy";
+			}else if(cloudCover >= 20){
+				icon = "partly cloudy";
+			}else{
+				icon = "clear";
+			}
+		}
+}
---
+++ src/com/ubcsolar/weather/ForecastIOFactory2.java
@@ -0,0 +1,97 @@
+package com.ubcsolar.weather;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.github.dvdme.ForecastIOLib.ForecastIO;
+import com.ubcsolar.Main.GlobalValues;
+
+public class ForecastIOFactory2 {
+	private static double latitude = 0;
+	private static double longitude = 0;
+	private static String timezone = "AMERICA";
+	private static int offset = -6;
+	private static List<JsonObject> datapoints;
+	
+	public static void addDatapoints(List<JsonObject> newDatapoints){
+		datapoints = new ArrayList<JsonObject>(newDatapoints);
+	}
+	
+	public static ForecastIO build(){
+		JsonObject forecastInfo = new JsonObject();
+		forecastInfo.add("latitude", latitude);
+		forecastInfo.add("longitude", longitude);
+		forecastInfo.add("timezone", timezone);
+		forecastInfo.add("offset", offset);
+		
+		forecastInfo.add("currently", datapoints.get(0));
+		
+		JsonObject hourlyForecast = new JsonObject();
+		hourlyForecast.add("summary", datapoints.get(0).get("summary"));
+		hourlyForecast.add("icon", datapoints.get(0).get("summary"));
+		
+		JsonArray hourlyArray = new JsonArray();
+		for(int i = 0; i < datapoints.size(); i++){
+			hourlyArray.add(datapoints.get(i));
+		}
+		
+		hourlyForecast.add("data", hourlyArray);
+		forecastInfo.add("hourly", hourlyForecast);
+		
+		forecastInfo.add("flags", buildFlagsObject());
+		
+		ForecastIO forecast = new ForecastIO(GlobalValues.WEATHER_KEY);
+		forecast.getForecast(forecastInfo);
+		
+		return forecast;
+	}
+	
+	private static JsonObject buildFlagsObject(){
+		JsonArray sourcesArray = new JsonArray();
+		sourcesArray.add("gfs");
+		sourcesArray.add("cmc");
+		sourcesArray.add("nam");
+		sourcesArray.add("rap");
+		sourcesArray.add("rtma");
+		sourcesArray.add("sref");
+		sourcesArray.add("fnmoc");
+		sourcesArray.add("isd");
+		sourcesArray.add("nwspa");
+		sourcesArray.add("madis");
+		
+		JsonArray isdArray = new JsonArray();
+		isdArray.add("712350-99999");
+		isdArray.add("713930-99999");
+		isdArray.add("718600-99999");
+		isdArray.add("718770-99999");
+		isdArray.add("718776-99999");
+		
+		JsonArray madisArray = new JsonArray();
+		madisArray.add("BLDQ1");
+		madisArray.add("CYBW");
+		madisArray.add("CYYC");
+		madisArray.add("D4846");
+		madisArray.add("D4993");
+		madisArray.add("D7650");
+		madisArray.add("E0424");
+		madisArray.add("E1159");
+		madisArray.add("E1808");
+		madisArray.add("E2194");
+		madisArray.add("E5375");
+		madisArray.add("E5393");
+		madisArray.add("E7122");
+		madisArray.add("E7132");
+		madisArray.add("NEIQ1");
+		madisArray.add("PRIQ1");
+		
+		JsonObject flags = new JsonObject();
+		flags.add("sources", sourcesArray);
+		flags.add("isd-stations", isdArray);
+		flags.add("madis-stations", madisArray);
+		flags.add("units", "si");
+		
+		return flags;
+	}
+}
