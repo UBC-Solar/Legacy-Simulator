7fc9c09b455d3c8449e75a77bfc55b481b758982
Hooman Vaseli <Hooman Vaseli>
2016-06-26 17:01:07 -0700

    CSVENTRY for Forecast Report  is roughly created. need more work

--- src/com/ubcsolar/common/ForecastReport.java
+++ src/com/ubcsolar/common/ForecastReport.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.github.dvdme.ForecastIOLib.FIODataBlock;
 import com.github.dvdme.ForecastIOLib.ForecastIO;
 
 public class ForecastReport extends DataUnit {
 	
-	public final static String classCSVHeaderRow =""; //TODO
+	private final static int numOfHours=4;
+	
+	private final static String temperatureCSVHeaderRow= "tempHr_0, tempHr_1, tempHr_2,tempHr_3";
+	private final static String cloudCoverCSVHeaderRow="cloudHr_0, cloudHr_1,cloudHr_2,cloudHr_3";
+	private final static String precipitationCSVHeaderRow="PrecipTypeHr_0,, PrecipTypeHr_1,,PrecipTypeHr_2,,PrecipTypeHr_3,";
+	private final static String dewPointCSVHeaderRow="DewPointHr_0, DewPointHr_1,DewPointHr_2,DewPointHr_3";
+	private final static String windSpeedCSVHeaderRow="WindSpeedHr_0, WindSpeedHr_1,WindSpeedHr_2,WindSpeedHr_3";
+
+	public final static String classCSVHeaderRow ="pointNum" + "," + temperatureCSVHeaderRow +","
+			+ cloudCoverCSVHeaderRow +"," + precipitationCSVHeaderRow +"," 
+			+ dewPointCSVHeaderRow +"," +windSpeedCSVHeaderRow; //TODO
+	
 	/**
 	 * turns the class fields into an entry for a csv file
 	 * see returnsEntireTable for info on row versus table
@@ -16,7 +28,61 @@ public class ForecastReport extends DataUnit {
 	 */
 	public String getCSVEntry()
 	{
-		return "NOT IMPLEMENTED";
+		String toReturn = "";
+		int entryNum = 0;
+		double tempDistance=0;
+		GeoCoord lastPoint = null;
+		double runningTotalDistance = 0;
+		
+		System.out.println("size of forecast:"+forecasts.size());
+		
+		//if (forecasts instanceof ForecastIO){  
+		
+			if (forecasts.size() != 0){
+				
+				for ( ForecastIO forecast : forecasts){
+					FIODataBlock temp = new FIODataBlock(forecast.getHourly());
+					
+					toReturn += entryNum +",";
+					
+					for (int i=0;i<numOfHours;i++){
+						toReturn += temp.datapoint(i).temperature() +",";
+						System.out.println("temperature is :" +temp.datapoint(i).temperature());
+					}
+					
+					for (int i=0;i<numOfHours;i++){
+						toReturn += (int) (temp.datapoint(i).cloudCover()*100) + "%" +",";
+						System.out.println("cloud % is:" +temp.datapoint(i).cloudCover()*100);
+					}
+					
+					for (int i=0;i<numOfHours;i++){
+						toReturn += temp.datapoint(i).precipType() +",";
+						toReturn += (int)(temp.datapoint(i).precipProbability()*100) + "%"+",";
+						System.out.println("Precipitation Type:" +temp.datapoint(i).precipType());
+						System.out.println("Chance of precipitation is:" +temp.datapoint(i).precipProbability()*100 +"%");
+					}
+					
+					for (int i=0;i<numOfHours;i++){
+						toReturn += temp.datapoint(i).dewPoint() +",";
+						System.out.println("DewPoint is:" +temp.datapoint(i).dewPoint());
+					}
+					
+					for (int i=0;i<numOfHours;i++){
+						toReturn += temp.datapoint(i).windSpeed()+",";
+						System.out.println("Wind Speed is:" +temp.datapoint(i).windSpeed());
+					}
+					
+					toReturn += "\r\n";
+					entryNum++;
+				}
+			}
+		//}
+		
+	//	else if(forecasts instanceof FIODataBlock){
+			
+	//	}
+
+		return toReturn;
 	}
 	
 	/**
@@ -34,7 +100,7 @@ public class ForecastReport extends DataUnit {
 	 */
 	public boolean returnsEntireTable ()
 	{
-		return false;
+		return true;
 	}
 
 	
