1f0ae7186febed4b1ea4cfdcd0253170f2f3155b
Candice Pang <candicekhpang@gmail.com>
2016-10-15 13:14:13 -0700

    Set First and Last rows to grow to fill window.

--- src/com/ubcsolar/ui/CarTelemPacketWindow.java
+++ src/com/ubcsolar/ui/CarTelemPacketWindow.java
@@ -66,7 +66,7 @@ public class CarTelemPacketWindow extends JFrame {
 		gridBagLayout.columnWidths = new int[]{0, 83, 0, 70, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblStateOfCharge = new JLabel("State Of Charge (%):");
