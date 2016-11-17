3697a193b6828ff05c16d6a46930b79418c42899
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-11-17 14:02:22 -0800

    added uid to new exception. This commit is stable (before testing)

--- src/com/ubcsolar/exception/NotEnoughChargeException.java
+++ src/com/ubcsolar/exception/NotEnoughChargeException.java
@@ -5,6 +5,8 @@ public class NotEnoughChargeException extends Exception{
 	public final double actualCharge;
 	public final double minCharge;
 	
+	private static final long serialVersionUID = 6745893214857964238L;
+	
 	public NotEnoughChargeException(double actualCharge, double minCharge, String message){
 		super(message);
 		this.actualCharge = actualCharge;
